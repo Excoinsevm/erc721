8742b9f314387a9f5194e7d317d91715ae1ca358
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-17 14:23:44 +0000

    Update parameters.ts

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -1,20 +1,15 @@
+import CustomChain from "../const/chain";
+
 export const contractConst = "0x5Baa53AfB5b14aA3e7c70D649D56cfa00BA324eE";
 
 export const chainConst = "CustomChain";
 
-// It is IMPORTANT to provide your own API key to use the thirdweb SDK and infrastructure.
-// Please ensure that you define the correct domain for your API key from the API settings page.
-// You can get one for free at https://thirdweb.com/create-api-key
-// Learn more here: https://blog.thirdweb.com/changelog/api-keys-to-access-thirdweb-infra
 export const clientIdConst = "31f32271c182cfb7a4ceb7e1ed655950";
 
-// Configure the primary color for buttons and other UI elements
 export const primaryColorConst = "blue";
 
-// Choose between "light" and "dark" mode
 export const themeConst = "dark";
 
-// Gasless relayer configuration options
 export const relayerUrlConst = ""; // OpenZeppelin relayer URL
 export const biconomyApiKeyConst = ""; // Biconomy API key
 export const biconomyApiIdConst = ""; // Biconomy API ID
